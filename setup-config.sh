9294fa2b632854a880e48e62645c538508944f11
Ethan Brooks <ethanabrooks@gmail.com>
2016-03-07 10:43:13 -0500

    about to pull

--- setup-config.sh
+++ setup-config.sh
@@ -3,10 +3,10 @@
 cd ~
 # download thoughbot dotfiles
 git clone git@github.com:thoughtbot/dotfiles.git
-# create symlinks in ~
-./link-config.bash
 # download vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# create symlinks in ~
+./link-config.bash
 # install plugins
 vim -E -c "PlugInstall" -c "qa!"
