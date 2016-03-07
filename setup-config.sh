58f5c14c08a565aed70d955da37d277189c387d0
Ethan <ethanabrooks@gmail.com>
2016-03-07 10:42:47 -0500

    added checks for if files already exist

--- setup-config.sh
+++ setup-config.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -ue
 
 cd ~
+
 # download thoughbot dotfiles
-git clone git@github.com:thoughtbot/dotfiles.git
+if ! [[ -e ~/dotfiles ]]  # check if already downloaded
+then
+  git clone git@github.com:thoughtbot/dotfiles.git
+fi
+
 # create symlinks in ~
 ./link-config.bash
+
 # download vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if ! [[ -e ~/.vim/autoload/plug.vim ]]  # check if already downloaded
+then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 # install plugins
 vim -E -c "PlugInstall" -c "qa!"
