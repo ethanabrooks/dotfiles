6a57611143e5a61ec8f66062f5693464a5839b0d
Ethan Brooks <ethanabrooks@gmail.com>
2016-03-08 09:30:21 -0500

    not sure

--- link-config.sh
+++ link-config.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -ue
 
-myconfig=$(find dotfiles-local -maxdepth 1 -type f -not -name '*.sh')  # mine
-dotfiles=$(find dotfiles -maxdepth 1 -name '*rc' -or -name '*rc.*')  # thoughtbot's
+function search-dotfiles {
+echo "find ~/dotfiles$1 -maxdepth 1"
+}
+$(search-dotfiles '')
+myconfig=$($(search-dotfiles -local) -type f -not -name '*.sh') # mine
+dotfiles=$($(search-dotfiles) -name '*rc' -or -name '*rc.*') # thoughtbot's
 files="$myconfig $dotfiles"
 
 for file in $files
 do
-  ln -f -s $file ~/.$(basename $file)
+  echo ln -f -s $file ~/.$(basename $file)
 done
 
