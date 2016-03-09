ba49c4ab6a2a42177efbac16d955d8327e3628d8
Ethan Brooks <ethanabrooks@gmail.com>
2016-03-09 09:58:05 -0500

    moved in check-nohup

--- link-config.sh
+++ link-config.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -ue
 
-function search-dotfiles {
-echo "find ~/dotfiles$1 -maxdepth 1"
+
+dotfiles=$HOME/dotfiles
+
+search-dotfiles() {
+  echo "find $dotfiles$1 -maxdepth 1"
 }
-$(search-dotfiles '')
-myconfig=$($(search-dotfiles -local) -type f -not -name '*.sh') # mine
-dotfiles=$($(search-dotfiles) -name '*rc' -or -name '*rc.*') # thoughtbot's
-files="$myconfig $dotfiles"
+dotfiles_local=$(search-dotfiles -local)
+echo $dotfiles_local
+myconfig=$($dotfiles_local -not -name '*.sh' -type f) # mine
+theirconfig=$($(search-dotfiles '') -name '*rc' -or -name '*rc.*') # thoughtbot's
+files="$myconfig $theirconfig"
 
 for file in $files
 do
-  echo ln -f -s $file ~/.$(basename $file)
+  ln -f -s $file $HOME/.$(basename $file)
 done
 
+# link dotfiles-local/bin
+ln -f -s $HOME/dotfiles-local/bin $HOME/bin
+
+# if bin is not in path, add it
+if [[ $SHELL == "/bin/bash" ]] && [[ ":$PATH:" != *":$HOME/bin:"* ]]
+then
+  echo 'export PATH=$HOME/bin:$PATH' >> $HOME/.bashrc
+fi
