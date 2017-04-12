1694ef33100aba1f406a244487e3930c236c517b
Ethan Brooks <ethanabrooks@gmail.com>
2017-04-12 17:11:21 -0400

    made link-setup interactive

--- link-config.sh
+++ link-config.sh
@@ -8,16 +8,23 @@ myconfig=$(find $base_dir -maxdepth 1 -type f -not -name '*.sh')  # mine
 dotfiles=$(find dotfiles -maxdepth 1 -name '*rc' -or -name '*rc.*')  # thoughtbot's
 files="$myconfig $dotfiles"
 
-for file in $files
-do
- ln -f -s $file $HOME/.$(basename $file)
- echo "ln -f -s $file $HOME/.$(basename $file)"
+for file in $files; do
+ ln -i -s $file $HOME/.$(basename $file)
+ echo "linked $file"
 done
 
-dirs=( git i3 ssh )
+dirs=( i3 ssh )
 dirs=( "${dirs[@]/#/$base_dir/}" )  # prepend base_dir
-for dir in ${dirs[@]}
-do
-  ln -s -f $dir $HOME/.$(basename $dir)
-  echo "ln -s -f $dir $HOME/.$(basename $dir)"
+for dir in ${dirs[@]}; do
+  dst_dir=$HOME/.$(basename $dir)
+  if [[ -d "$dst_dir" ]]; then
+    echo "$dst_dir already exists. Linking contents"
+    for file in $(ls $dir); do
+      ln -s -i $dir/$file $dst_dir/$file
+      echo "linked $file"
+    done
+  else
+    ln -s -i $dir $dst_dir
+    echo "linked $dir"
+  fi
 done
