6ce6a72cea3150f1c788d263b726f1a2f2064bba
Ethan Brooks <ethanabrooks@gmail.com>
2017-09-13 18:34:01 -0400

    fixed up link-config using shellchecker

--- setup-config.sh
+++ setup-config.sh
@@ -14,32 +14,32 @@ privileged='y'
 if [[ $privileged == 'y' ]]; then
   sudo pacman -Sy curl vim zsh python-pip the_silver_searcher tree termite
   sudo pip install virtualenvwrapper
-  mkdir -p $HOME/virtualenvs
+  mkdir -p "$HOME/virtualenvs"
 
   print 'Cloning zsh-syntax-highlighting...'
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting || :
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/zsh-syntax-highlighting" || :
 
-  chsh -s $(which zsh)
+  chsh -s "$(which zsh)"
 
 fi
 
 print 'Downloading vim-plug...'
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 print 'Cloning gruvbox'
-git clone https://github.com/morhetz/gruvbox.git $HOME/.vim/bundle/gruvbox || :
+git clone https://github.com/morhetz/gruvbox.git "$HOME/.vim/bundle/gruvbox" || :
 
 # hack to fix file encoding in vimrc
 print 'Setting encodings to utf-8...'
-sed -i '1iscriptencoding utf-8\nset encoding=utf-8' $HOME/.vimrc
+sed -i '1iscriptencoding utf-8\nset encoding=utf-8' "$HOME/.vimrc"
 
 exit
 print 'Installing vim plugins...'
 vim -c PlugInstall -c qall
 
 print 'Linking local dotfiles'
-bash $HOME/dotfiles/link-config.sh
+bash "$HOME/dotfiles/link-config.sh"
 
 GREEN='\033[0;32m'
 print "${GREEN}All done! Congratulations, your system is all setup."
