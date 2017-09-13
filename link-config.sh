73f6d8bca3cf880d6b6305bbe5b74662f8abd913
Ethan Brooks <ethanabrooks@gmail.com>
2017-09-13 18:34:01 -0400

    fixed up link-config using shellchecker

--- link-config.sh
+++ link-config.sh
@@ -2,23 +2,23 @@
 
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-files=$(find $base_dir -maxdepth 1 -type f -not -name '*.sh')
+files=$(find "$base_dir" -maxdepth 1 -type f -not -name '*.sh')
 
 for file in $files
 do
-  ln -fs $file $HOME/.$(basename $file)
+  ln -fs "$file" "$HOME/.$(basename "$file")"
 done
 
 dirs="git"
 
 for dir in $dirs 
 do
-  ln -sf $base_dir/$dir $HOME/.$(basename $dir)
+  ln -sf "$base_dir"/"$dir" "$HOME/.$(basename $dir)"
 done
 
 configs="i3 polybar termite"
 
 for config in $configs 
 do
-  ln -sf $base_dir/$config $HOME/.config/$(basename $config)
+  ln -sf "$base_dir"/"$config" "$HOME/.config/$(basename "$config")"
 done
